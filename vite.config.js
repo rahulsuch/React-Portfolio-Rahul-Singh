5ef9d35c5aa8a5805c3d143c3fa28f50b61cc8b0
Rahul Singh <builtitfunny@gmail.com>
2025-11-07 17:46:28 +0530

    fixed error for vercel deployment added path specified for ESM on vercel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Define __dirname in ESM (Vercel uses pure ESM)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
